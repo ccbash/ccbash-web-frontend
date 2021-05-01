f7032ec40a33684410126408a8c8612d0ee99417
Kai Grotelüschen <kgr@ccbash.de>
2021-05-01 23:20:09 +0200

    modded the setup

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,51 +1,51 @@
 import React from "react";
-import { Link, StaticQuery, graphql } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 const Nav = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        strapiGlobal {
-          siteName
-        }
-        allStrapiCategory {
-          edges {
-            node {
-              slug
-              name
-            }
-          }
+  const data = useStaticQuery(query);
+
+  return (
+    <header id="nav-open">
+      <Link to="/">{data.strapiGlobal.siteName}</Link>
+
+      <nav>
+        <ul>
+          {data.allStrapiCategory.edges.map((category, i) => (
+          <li key={`category__${category.node.slug}`}>
+            <Link to={`/category/${category.node.slug}`}>
+              {category.node.name}
+            </Link>
+          </li>
+          ))}
+
+        </ul>
+      </nav>
+
+      <Link class="nav-toggle nav-open" to="#nav-open">
+        <i class="fas fa-bars"/>
+      </Link>
+	  <Link class="nav-toggle nav-closed" to="#nav-closed">
+	    <i class="fas fa-times"/>
+	  </Link>
+
+    </header>
+  )
+);
+
+const query = graphql`
+  query {
+    strapiGlobal {
+      siteName
+    }
+    allStrapiCategory {
+      edges {
+        node {
+          slug
+          name
         }
       }
-    `}
-    render={(data) => (
-
-      <header id="nav-open">
-        <Link to="/">{data.strapiGlobal.siteName}</Link>
-
-        <nav>
-          <ul>
-            {data.allStrapiCategory.edges.map((category, i) => (
-            <li key={`category__${category.node.slug}`}>
-              <Link to={`/category/${category.node.slug}`}>
-                {category.node.name}
-              </Link>
-            </li>
-            ))}
-
-          </ul>
-        </nav>
-
-        <Link class="nav-toggle nav-open" to="#nav-open">
-          <i class="fas fa-bars"/>
-        </Link>
-	    <Link class="nav-toggle nav-closed" to="#nav-closed">
-	      <i class="fas fa-times"/>
-	    </Link>
-
-      </header>
-    )}
-  />
-);
+    }
+  }
+`;
 
 export default Nav;

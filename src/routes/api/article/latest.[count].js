c49f6798ff924d95b2c7d88e2936be8fcb7fc443
Kai Grotelueschen <kai.grotelueschen@senacor.com>
2021-09-05 16:50:43 +0200

    linting should work now

--- src/routes/api/article/latest.[count].js
+++ src/routes/api/article/latest.[count].js
@@ -1,11 +1,12 @@
 import { client } from '$lib/client.js'
 import { gql } from '@apollo/client/core/core.cjs.js'
 
-export async function get({ params }) {
-  const { count } = params
-  const cint = parseInt(count)
-  //const count = 3
-  console.log('Query API: latest articles, count: ' + cint)
+// export async function get({ params }) {
+//const { count } = params
+//const cint = parseInt(count)
+export async function get() {
+  const count = 3
+  console.log('Query API: latest articles, count: ' + count)
   const query = gql`
     query LatestArticleLimted($count: Int!) {
       articles(limit: $count, sort: "created_at:desc") {
@@ -25,7 +26,7 @@ export async function get({ params }) {
   `
 
   try {
-    const result = await client.query({ query, variables: { cint } })
+    const result = await client.query({ query, variables: { count } })
     return {
       status: 200,
       body: result.data
